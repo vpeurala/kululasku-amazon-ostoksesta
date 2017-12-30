c42917245c5ee9f403ec3e99e368e52f6fc4fac5
Ville Peurala <ville.peurala@gmail.com>
2017-12-31 00:32:24 +0200

    Wrote a unit test for pdf parsing.

--- src/parse_pdf.js
+++ src/parse_pdf.js
@@ -3,12 +3,22 @@
 const child_process = require("child_process");
 
 function parse_price_in_usd_and_item_name_from_pdf(pdf_file) {
-    child_process.execSync("pdftotext " +
+    let child_process_output = child_process.execSync("pdftotext " +
         pdf_file +
-        " - | ggrep -e 'Order Total:' -e 'Items Ordered' " +
+        " - | grep -e 'Order Total:' -e 'Items Ordered' " +
         "| sed 's/^.*Order Total: \\$//g' " +
         "| sed 's/^Items Ordered //g' " + "" +
-        "| sed 's/ Quantity:.*$//g'");
+        "| sed 's/ Quantity:.*$//g'", {
+        "encoding": "ASCII",
+        "timeout": 1000
+    });
+    let child_process_output_lines = child_process_output.split("\n");
+    let price_in_usd = child_process_output_lines[0].trim();
+    let product_name = child_process_output_lines[1].trim();
+    return {
+        "price_in_usd": price_in_usd,
+        "product_name": product_name
+    };
 }
 
 module.exports = {
---
+++ src/unit_tests.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const test = require("tape");
+
+const parse_pdf = require("./parse_pdf");
+
+test("pdf_parsing", (t) => {
+    t.plan(1);
+    let expected = {
+        "price_in_usd": "22.75",
+        "product_name": "PostgreSQL: Up and Running: A Practical Guide to the Advanced Open Source Database[Kindle Edition] By: Regina O. Obe, Leo S. Hsu"
+    };
+    let actual = parse_pdf.parse_price_in_usd_and_item_name_from_pdf("./test_resources/Amazon_Invoice.pdf");
+    t.deepEqual(actual, expected);
+});
