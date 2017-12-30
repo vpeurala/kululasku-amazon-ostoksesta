c093977421abc051ec89217ad142bfa9c59dd4cb
Ville Peurala <ville.peurala@gmail.com>
2017-12-30 23:01:28 +0200

    Extract selectors to their own file. Selecting from dropdowns works now.

--- src/create_kululasku.js
+++ src/create_kululasku.js
@@ -1,8 +1,7 @@
 "use strict";
 
-const testcafe = require("testcafe/lib/api/exportable-lib");
-const selector = testcafe.Selector;
 const etasku_front_page_url = "https://www.etasku.fi/";
+const s = require("./selectors");
 const username = process.env.ETASKU_USERNAME;
 const password = process.env.ETASKU_PASSWORD;
 const receipt_file = process.env.ETASKU_RECEIPT_FILE;
@@ -10,33 +9,46 @@ const receipt_file = process.env.ETASKU_RECEIPT_FILE;
 fixture("Create kululasku").page(etasku_front_page_url);
 
 test("Happy path", async (t) => {
-    console.log("username: ", username);
-    console.log("password: ", password);
-    console.log("receipt file: ", receipt_file);
     await t
-        .click(selector("ul.nav.navbar-nav.pull-right.hidden-xs a#navbar-login.btn.btn-primary"))
-        .typeText(selector("input#username.textbox"), username, {"replace": true})
-        .typeText(selector("input.textbox[type=password]"), password, {"replace": true})
-        .click(selector("input.loginbutton"))
-        .click(selector("div#customertools.actionbutton"))
+        .click(s.kirjaudu)
+        .typeText(s.kayttajatunnus, username, {"replace": true})
+        .typeText(s.salasana, password, {"replace": true})
+        .click(s.kirjaudu_sisaan)
+        .click(s.luo_tosite)
         // Kuittitiedoston uploadaus.
-        .setFilesToUpload("#uploadifive-fileDropZone input[type=file]", receipt_file)
-        // Ostopäivä-kenttä jätetään default-arvoon, eli siihen päivään jolloin tämä ajetaan; muista tehdä kululasku heti ostoksen jälkeen!
+        .setFilesToUpload(s.lisaa_tiedosto, receipt_file)
+        // Ostopäivä-kenttä jätetään default-arvoon, eli siihen päivään jolloin tämä ajetaan;
+        // muista tehdä kululasku heti ostoksen jälkeen!
         // Ostopaikka-kenttä = input#receipt_name.
-        .typeText(selector("input#receipt_name"), "amazon.com")
+        .typeText(s.ostopaikka, "amazon.com")
         // Lisätietoa-kenttä = textarea#show_comment_edit.
-        .typeText(selector("textarea#show_comment_edit"), "Ammattikirjallisuutta: TODO")
+        .typeText(s.lisatietoa, "Ammattikirjallisuutta: TODO")
         // Hinta-kenttä (euroina) = input#show_price_edit.
-        .typeText(selector("input#show_price_edit"), "TODO")
+        .typeText(s.hinta, "TODO")
         // Verokanta-kenttä = select#taxrate_select; Amazonilta ostettaessa aina "24 %".
-        // .click(selector("select#taxrate_select").filter) // TODO Kesken
+        .click(s.verokanta)
+        .click(s.verokanta.find("option").withText("24 %"))
         // Luokitus-kenttä = select#class_select; kirjaostoksissa "Työkalut".
+        .click(s.luokitus)
+        .click(s.luokitus.find("option").withText("Työkalut"))
         // Maksutapa-kenttä = select#paymethod_select; Amazonilta ostettaessa ainakin minulla aina "Luottokortti".
+        .click(s.maksutapa)
+        .click(s.maksutapa.find("option").withText("Luottokortti"))
         // Kustannuspaikka-kenttä = select#Kustannuspaikka_select.dimensiongroup_select; aina "1999 Liiketoiminta".
+        .click(s.kustannuspaikka)
+        .click(s.kustannuspaikka
+            .find("option").withText("1999 Liiketoiminta"))
         // Toimiala-kenttä = select#Toimiala_select.dimensiongroup_select; aina "IT IT".
+        .click(s.toimiala)
+        .click(s.toimiala
+            .find("option").withText("IT IT"))
         // Alue-kenttä = select#Alue_select.dimensiongroup_select; aina "10091 Helsinki".
+        .click(s.alue)
+        .click(s.alue.find("option").withText("10091 Helsinki"))
         // Palvelu-kenttä = select#Palvelu_select.dimensiongroup_select; aina "KON Konsultointi".
+        .click(s.palvelu)
+        .click(s.palvelu.find("option").withText("KON Konsultointi"))
         // Tositenippu-kenttä jätetään default-arvoon "Ei valittu".
-        .click(selector("input#save_button"))
+        .click(s.tallenna)
         .debug();
 });
---
+++ src/selectors.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const testcafe = require("testcafe/lib/api/exportable-lib");
+const selector = testcafe.Selector;
+
+// Etusivu
+const kirjaudu = selector("ul.nav.navbar-nav.pull-right.hidden-xs a#navbar-login.btn.btn-primary");
+
+// Sisäänkirjautumissivu
+const kayttajatunnus = selector("input#username.textbox");
+const salasana = selector("input.textbox[type=password]");
+const kirjaudu_sisaan = selector("input.loginbutton");
+
+// Kululaskun syöttösivu
+const luo_tosite = selector("div#customertools.actionbutton");
+const lisaa_tiedosto = selector("#uploadifive-fileDropZone input[type=file]");
+const ostopaikka = selector("input#receipt_name");
+const lisatietoa = selector("textarea#show_comment_edit");
+const hinta = selector("input#show_price_edit");
+const verokanta = selector("select#taxrate_select");
+const luokitus = selector("select#class_select");
+const maksutapa = selector("select#paymethod_select");
+const kustannuspaikka = selector("select#Kustannuspaikka_select.dimensiongroup_select");
+const toimiala = selector("select#Toimiala_select.dimensiongroup_select");
+const alue = selector("select#Alue_select.dimensiongroup_select");
+const palvelu = selector("select#Palvelu_select.dimensiongroup_select");
+const tallenna = selector("input#save_button");
+
+module.exports = {
+    "kirjaudu": kirjaudu,
+    "kayttajatunnus": kayttajatunnus,
+    "salasana": salasana,
+    "kirjaudu_sisaan": kirjaudu_sisaan,
+    "luo_tosite": luo_tosite,
+    "lisaa_tiedosto": lisaa_tiedosto,
+    "ostopaikka": ostopaikka,
+    "lisatietoa": lisatietoa,
+    "hinta": hinta,
+    "verokanta": verokanta,
+    "luokitus": luokitus,
+    "maksutapa": maksutapa,
+    "kustannuspaikka": kustannuspaikka,
+    "toimiala": toimiala,
+    "alue": alue,
+    "palvelu": palvelu,
+    "tallenna": tallenna
+};
