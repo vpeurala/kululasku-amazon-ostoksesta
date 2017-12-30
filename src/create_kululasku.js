80f8cdd34c7ec79a355c96f38ef0306b490a3c1c
Ville Peurala <ville.peurala@gmail.com>
2017-12-30 19:22:02 +0200

    Use 'kululasku' instead of 'kulari'.

--- src/create_kululasku.js
+++ src/create_kululasku.js
@@ -7,5 +7,5 @@ const etasku_front_page_url = "https://www.etasku.fi/";
 fixture("Create kululasku").page(etasku_front_page_url);
 
 test("Happy path", async (t) => {
-    await t.click("a#navbar-login.btn");
+    await t.click(selector("a#navbar-login"));
 });
