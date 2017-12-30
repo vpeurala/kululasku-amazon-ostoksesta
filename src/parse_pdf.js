bbf66db8a25a0ea212e26bc7f85882e1d8e70cc9
Ville Peurala <ville.peurala@gmail.com>
2017-12-31 00:51:42 +0200

    Parse dates correctly. Add dependency to moment.js for that.

--- src/parse_pdf.js
+++ src/parse_pdf.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const child_process = require("child_process");
+const moment = require("moment");
 
 function parse_price_in_usd_and_item_name_from_pdf(pdf_file) {
     let child_process_output = child_process.execSync("pdftotext " +
@@ -15,12 +16,15 @@ function parse_price_in_usd_and_item_name_from_pdf(pdf_file) {
     });
     let child_process_output_lines = child_process_output.split("\n");
     let price_in_usd = child_process_output_lines[0].trim();
-    let purchase_date = child_process_output_lines[1].trim();
+    let purchase_date_in_wrong_format = child_process_output_lines[1].trim();
+    let purchase_date_in_correct_format =
+        moment(purchase_date_in_wrong_format, "MMMM D, YYYY")
+            .format("D.M.YYYY");
     let product_name = child_process_output_lines[2].trim();
     return {
         "price_in_usd": price_in_usd,
         "product_name": product_name,
-        "purchase_date": purchase_date
+        "purchase_date": purchase_date_in_correct_format
     };
 }
 
