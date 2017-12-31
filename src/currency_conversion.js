de1845ac04879751ba5145c65ac1d8aca31c6492
Ville Peurala <ville.peurala@gmail.com>
2017-12-31 02:34:53 +0200

    Nock is added to the project, but not used yet, so every run of unit tests (tape) hits the real currencylayer.com service, using up our monthly quota.

--- src/currency_conversion.js
+++ src/currency_conversion.js
@@ -1,29 +1,26 @@
 "use strict";
 
-const https = require("https");
+const request = require("sync-request");
 
 const CURRENCYLAYER_COM_API_KEY = "737e36a2d5d98271355df3729abbec59";
 
-async function usd_to_eur(usd_amount) {
-    await https.get("https://currencylayer.com/api/convert" +
+function get_exchange_rate_from_currency_layer() {
+    let response = request("GET", "http://apilayer.net/api/live" +
         "?access_key=" + CURRENCYLAYER_COM_API_KEY +
-        "&from=USD" +
-        "&to=EUR" +
-        "&amount=" + usd_amount, (response) => {
-        let data = "";
-        // TODO Remove
-        console.log("response: ", response);
-        response.on("data", (chunk) => {
-            data += chunk;
-        });
-        response.on("end", () => {
-            return JSON.parse(data);
-        });
-    }).on("error", (e) => {
-        // eslint-disable-next-line no-console
-        console.error("Error from currencylayer.com: ", e);
-        throw e;
-    });
+        "&currencies=USD,EUR" +
+        "&format=0");
+    let body = response.getBody("ASCII");
+    let json = JSON.parse(body);
+    if (json.success !== true) {
+        throw "Getting exchange rates from currencylayer.com was not successful. " +
+        "Response was: " + json;
+    }
+    return json.quotes.USDEUR;
+}
+
+function usd_to_eur(usd_amount) {
+    let exchange_rate_usd_to_eur = get_exchange_rate_from_currency_layer();
+    return usd_amount * exchange_rate_usd_to_eur;
 }
 
 module.exports = {
--- src/unit_tests.js
+++ src/unit_tests.js
@@ -1,16 +1,29 @@
 "use strict";
+/* eslint-disable no-magic-numbers */
 
 const test = require("tape");
+const nock = require("nock");
 
+const currency_conversion = require("./currency_conversion");
 const parse_pdf = require("./parse_pdf");
+
+const expected_parse_result = {
+    "price_in_usd": "22.75",
+    "product_name": "PostgreSQL: Up and Running: A Practical Guide to the Advanced Open Source Database[Kindle Edition] By: Regina O. Obe, Leo S. Hsu",
+    "purchase_date": "14.12.2017"
+};
 
 test("pdf_parsing", (t) => {
     t.plan(1);
-    let expected = {
-        "price_in_usd": "22.75",
-        "product_name": "PostgreSQL: Up and Running: A Practical Guide to the Advanced Open Source Database[Kindle Edition] By: Regina O. Obe, Leo S. Hsu",
-        "purchase_date": "14.12.2017"
-    };
     let actual = parse_pdf.parse_price_in_usd_and_item_name_from_pdf("./test_resources/Amazon_Invoice.pdf");
+    let expected = expected_parse_result;
+    t.deepEqual(actual, expected);
+});
+
+test("currency conversion", (t) => {
+    t.plan(1);
+    nock("http://apilayer.net");
+    let expected = 18.955391;
+    let actual = currency_conversion.usd_to_eur(parseFloat(expected_parse_result.price_in_usd));
     t.deepEqual(actual, expected);
 });
